9ceed4b9985fd40d813c1aa04aad0c876d1936e5
Rachel Castro <rcastro@myassetmap.com>
2024-01-29 12:19:45 -0700

    added numpy

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+import numpy as np
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def import_info(filename):
+    #store info in matrix
+    x = 1
+
+def gen_occupations(nodes):
+    return 0
+
+def gen_moves(occup, edge_matrix, node_colors):
+    return 0
+
+def create_graph(occup, moves):
+    return 0
+
+
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
