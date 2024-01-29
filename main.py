dcfd2e41314f2752ff661d69a6f565842c75b8d4
Rachel Castro <rcastro@myassetmap.com>
2024-01-29 12:33:12 -0700

    Command line file input implementation

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-import numpy as np
+import networkx as nx
+import sys
+import fileinput
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
-def import_info(filename):
-    #store info in matrix
-    x = 1
+def file_import():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <path_to_input_file>")
+        sys.exit(1)
+
+    input_file_path = sys.argv[1]
+
+    try:
+        # Use fileinput.input() to handle both stdin and file input
+        with fileinput.input(files=(input_file_path,)) as f:
+            input_lines = f.readlines()
+            process_input(input_lines)
+
+    except FileNotFoundError:
+        print(f"Error: File '{input_file_path}' not found.")
+        sys.exit(1)
+
+def process_input(input_lines):
+    return 0
 
 def gen_occupations(nodes):
     return 0
@@ -15,14 +32,17 @@ def gen_moves(occup, edge_matrix, node_colors):
     return 0
 
 def create_graph(occup, moves):
+    G = nx.Graph()
+
     return 0
 
 
-
+def main():
+    file_import()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 
